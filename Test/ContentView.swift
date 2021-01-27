ff81a497759c5f439f53ac45706a86c90c9d6054
Abhiram13 <abhiram131869@gmail.com>
2021-01-27 22:09:20 +0530

    added HStack inside VStack

--- Test/ContentView.swift
+++ Test/ContentView.swift
@@ -10,13 +10,18 @@ import CoreData
 
 struct ContentView: View {
     private let arrays: [String] = ["one", "two", "three"];
+    private var rows: [Int] = [1, 2, 3, 4, 5, 6, 7];
     private var B: Color = .blue;
     private var screenWidth = UIScreen.main.bounds.width;
     private var screenHeight = UIScreen.main.bounds.height;
     var body : some View {
-        VStack(alignment: .center, spacing: 10) {
-            ForEach(arrays, id: \.self) {
-                array in Text("Hello " + array)
+        VStack (alignment: .leading, spacing: 10) {
+            ForEach(rows, id: \.self) {
+                row in HStack(alignment: .top, spacing: 40) {
+                    ForEach(arrays, id: \.self) {
+                        array in Text("Hello " + array)
+                    }
+                }
             }
         }.frame(width: screenWidth, height: screenHeight, alignment: .center)
         .foregroundColor(B)
