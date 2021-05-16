ae88aa8e6ccfeea509031e29ee33f81f50303d54
Abhiram13 <abhiram131869@gmail.com>
2021-05-16 18:24:08 +0530

    initial commit

--- Calculator/Detail.swift
+++ Calculator/Detail.swift
@@ -8,7 +8,25 @@
 import SwiftUI;
 
 struct Detail: View {
+    private let screenWidth: CGFloat = UIScreen.main.bounds.width;
+    private let screenHeight: CGFloat = UIScreen.main.bounds.height;
+    
     var body: some View {
-        Text("Hello World");
+//        VStack(spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/) {
+//
+//
+//        }
+        Text("Percentage Calculator")
+            .bold()
+            .font(.system(size: 20))
+            .frame(alignment: .trailing)
+        
+        Text("Some Random Text")
+            .font(.system(size: 15))
+            .frame(alignment: .leading)
+        
+        Text("Another Random Text")
+            .font(.system(size: 15))
+            .frame(alignment: .center)
     }
 }
