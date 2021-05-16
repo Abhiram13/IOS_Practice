529c2321324c8c4852581ac3e9b788a5edfc2333
Abhiram13 <abhiram131869@gmail.com>
2021-05-16 18:43:13 +0530

    added background color to the view

--- Calculator/Detail.swift
+++ Calculator/Detail.swift
@@ -12,21 +12,21 @@ struct Detail: View {
     private let screenHeight: CGFloat = UIScreen.main.bounds.height;
     
     var body: some View {
-//        VStack(spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/) {
-//
-//
-//        }
-        Text("Percentage Calculator")
-            .bold()
-            .font(.system(size: 20))
-            .frame(alignment: .trailing)
-        
-        Text("Some Random Text")
-            .font(.system(size: 15))
-            .frame(alignment: .leading)
-        
-        Text("Another Random Text")
-            .font(.system(size: 15))
-            .frame(alignment: .center)
+        Color.green.overlay(
+            VStack(spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/) {
+                Text("Percentage Calculator")
+                    .bold()
+                    .font(.system(size: 20))
+                    .frame(alignment: .trailing)
+                
+                Text("Some Random Text")
+                    .font(.system(size: 15))
+                    .frame(alignment: .leading)
+                
+                Text("Another Random Text")
+                    .font(.system(size: 15))
+                    .frame(alignment: .center)
+            }
+        ).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
