ccf019709a2bcf663dfb025d0add33d7642c5b20
Abhiram13 <abhiram131869@gmail.com>
2021-05-16 10:31:15 +0530

    added view controller

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -12,15 +12,74 @@ func integerRows() -> [Int] {
     return [1, 2, 3, 4, 5, 6, 7];
 }
 
+
+class ViewController: UIViewController {
+    let scrollView = UIScrollView()
+    let contentView = UIView()
+            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupScrollView()
+        setupViews()
+    }
+        
+    func setupScrollView() {
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        scrollView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        contentView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
+        contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
+        contentView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+    }
+        
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        label.numberOfLines = 0
+        label.sizeToFit()
+        label.textColor = UIColor.white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+        
+    let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+        label.numberOfLines = 0
+        label.sizeToFit()
+        label.textColor = UIColor.white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+        
+    func setupViews(){
+        contentView.addSubview(titleLabel)
+        titleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        titleLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3/4).isActive = true
+        contentView.addSubview(subtitleLabel)
+        subtitleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 25).isActive = true
+        subtitleLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3/4).isActive = true
+        subtitleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+    }
+}
+
 struct ContentView: View {
     private var arrays: [String] = ["one", "two", "three"];
 //    private var rows: [Int] = [1, 2, 3, 4, 5, 6, 7];
     private var B: Color = .blue;
-    private var screenWidth = UIScreen.main.bounds.width;
-    private var screenHeight = UIScreen.main.bounds.height;
-    @State var showsAlert = false;
-    @State var username: String = ""
-        
+    private let screenWidth: CGFloat = UIScreen.main.bounds.width;
+    private let screenHeight: CGFloat = UIScreen.main.bounds.height;
+    @State private var showsAlert: Bool = false;
+    @State private var username: String = "";
+
     var body : some View {
         VStack (spacing: 10) {
             ForEach(integerRows(), id: \.self) {
@@ -30,17 +89,19 @@ struct ContentView: View {
                     }
                 }
             }
-            
-            TextField("User name (email address)", text: $username)
-                .autocapitalization(.none)
+
+            TextField("First Number", text: $username)
+                .autocapitalization(.allCharacters)
                 .disableAutocorrection(true)
-                .border(Color(UIColor.separator))
-                Text(username)
-                    .foregroundColor(.blue)
-                
-            Button(action: { self.showsAlert.toggle() }) {
-                Text("Show Alert")
-            }.alert(isPresented: self.$showsAlert) {
+                .keyboardType(.namePhonePad)
+                .frame(width: screenWidth - 50, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 1)
+                .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+
+            Text(username).foregroundColor(.blue)
+
+            Button(action: { self.showsAlert.toggle() }) { Text("Show Alert") }
+                .alert(isPresented: self.$showsAlert) {
                 Alert(title: Text("Hello"))
             }
         }.frame(width: screenWidth, height: screenHeight, alignment: .top)
