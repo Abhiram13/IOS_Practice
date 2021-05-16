de2c326e47b9a4d90756c6d3c44bfb09ec9e5eb0
Abhiram13 <abhiram131869@gmail.com>
2021-05-16 19:32:08 +0530

    commit

--- Calculator/Detail.swift
+++ Calculator/Detail.swift
@@ -12,21 +12,23 @@ struct Detail: View {
     private let screenHeight: CGFloat = UIScreen.main.bounds.height;
     
     var body: some View {
-        Color.green.overlay(
-            VStack(spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/) {
-                Text("Percentage Calculator")
-                    .bold()
-                    .font(.system(size: 20))
-                    .frame(alignment: .trailing)
-                
-                Text("Some Random Text")
-                    .font(.system(size: 15))
-                    .frame(alignment: .leading)
-                
-                Text("Another Random Text")
-                    .font(.system(size: 15))
-                    .frame(alignment: .center)
-            }
-        ).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        Color.white.overlay(
+            VStack(alignment: .leading, spacing: 10) {
+                VStack (alignment: .leading, spacing: nil) {
+                    Text("Percentage Calculator")
+                        .bold()
+                        .font(.system(size: 25))
+                    
+                    Text("Some Random Text")
+                        .font(.system(size: 15))
+                    
+                    Text("Another Random Text")
+                        .font(.system(size: 15))
+                        .frame(alignment: .center)
+                }.frame(width: screenWidth - 35, height: screenHeight - 80, alignment: .topLeading)
+//                .border(Color.green, width: 1)
+            }.frame(width: screenWidth, height: screenHeight)
+//            .border(Color.black, width: 1)
+        )
     }
 }
