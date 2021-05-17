f724ac873e043b2375f2550c41d5911e8b0cc279
Abhiram13 <abhiram131869@gmail.com>
2021-05-17 14:25:49 +0530

    commit

--- Calculator/Detail.swift
+++ Calculator/Detail.swift
@@ -7,10 +7,19 @@
 
 import SwiftUI;
 
+func CalculatePercentage(total: String, percentage: String) -> Int {
+    let Total: Int = Int(total) ?? 0
+    let Percentage: Int = Int(percentage) ?? 0
+    
+    return (Total/100 * Percentage);
+}
+
 struct Detail: View {
     private let screenWidth: CGFloat = UIScreen.main.bounds.width;
     private let screenHeight: CGFloat = UIScreen.main.bounds.height;
     @State private var fNum: String = "";
+    @State private var lNum: String = "";
+    @State var showsAlert = false;
     
     // (Total)/100 * (percentage) --- to find how much value the percentage presents ------ Find Value
     // (Value)/(Total) * 100 --- to find out how much percentage the value covers ------ Find Percentage
@@ -29,7 +38,7 @@ struct Detail: View {
                         .font(.system(size: 20))
                     
                     Text("Find how much value the percentage presents")
-                        .font(.system(size: 17))
+                        .font(.system(size: 15))
                     
                     VStack(alignment: .center, spacing: 10) {
                         TextField("Total Amount", text: $fNum)
@@ -39,12 +48,18 @@ struct Detail: View {
                             .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
                             .multilineTextAlignment(.center)
                         
-                        TextField("Percentage %", text: $fNum)
+                        TextField("Percentage %", text: $lNum)
                             .keyboardType(.namePhonePad)
                             .frame(width: screenWidth - 50, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                             .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 1)
                             .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
                             .multilineTextAlignment(.center)
+                        
+                        Button(action: { self.showsAlert.toggle() }) {
+                            Text("Calculate")
+                        }.alert(isPresented: self.$showsAlert) {
+                            Alert(title: Text("\(CalculatePercentage(total: fNum, percentage: lNum))"))
+                        }
                         
                     }.frame(width: screenWidth - 45, height: screenHeight - 50, alignment: .top)
                 }.frame(width: screenWidth - 35, height: screenHeight - 80, alignment: .topLeading)
