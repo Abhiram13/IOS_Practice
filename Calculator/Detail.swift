4693bf8b8e72c2626515d20db51e1e9eb8bccbd5
Abhiram13 <abhiram131869@gmail.com>
2021-05-17 15:52:56 +0530

    commit

--- Calculator/Detail.swift
+++ Calculator/Detail.swift
@@ -24,6 +24,12 @@ func CalculateValue(total: String, value: String) -> Double {
     return result * 100;
 }
 
+func demoView() -> some View {
+    return VStack(spacing: nil) {
+        Text("This is From Sepertae Function")
+    }
+}
+
 struct Detail: View {
     private let screenWidth: CGFloat = UIScreen.main.bounds.width;
     private let screenHeight: CGFloat = UIScreen.main.bounds.height;
@@ -38,88 +44,92 @@ struct Detail: View {
     // (Value)/(Total) * 100 --- to find out how much percentage the value covers ------ Find Percentage
     
     var body: some View {
-        Color.white.overlay(
-            VStack(alignment: .leading, spacing: 10) {
-                VStack (alignment: .leading, spacing: nil) {
-                    Text("Percentage Calculator")
-                        .bold()
-                        .frame(minHeight: 60)
-                        .font(.system(size: 25))
-                    
-                    Group {
-                        Text("Find Value")
+        ScrollView {
+            Color.white.overlay(
+                VStack(alignment: .leading, spacing: 10) {
+                    VStack (alignment: .leading, spacing: nil) {
+                        Text("Percentage Calculator")
                             .bold()
-                            .font(.system(size: 20))
+                            .frame(minHeight: 60)
+                            .font(.system(size: 25))
                         
-                        Text("Find how much value the percentage presents")
-                            .font(.system(size: 15))
-                        
-                        VStack(alignment: .center, spacing: 10) {
-                            TextField("Total Amount", text: $fNum)
-                                .keyboardType(.namePhonePad)
-                                .frame(width: screenWidth - 50, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 1)
-                                .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
-                                .multilineTextAlignment(.center)
+                        Group {
+                            Text("Find Value")
+                                .bold()
+                                .font(.system(size: 20))
                             
-                            TextField("Percentage %", text: $lNum)
-                                .keyboardType(.namePhonePad)
-                                .frame(width: screenWidth - 50, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 1)
-                                .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
-                                .multilineTextAlignment(.center)
+                            Text("Find how much value the percentage presents")
+                                .font(.system(size: 15))
                             
-                            Button(action: { self.showsAlert.toggle() }) {
-                                Text("Calculate")
+                            VStack(alignment: .center, spacing: 10) {
+                                TextField("Total Amount", text: $fNum)
+                                    .keyboardType(.namePhonePad)
+                                    .frame(width: screenWidth - 50, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 1)
+                                    .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+                                    .multilineTextAlignment(.center)
+                                
+                                TextField("Percentage %", text: $lNum)
+                                    .keyboardType(.namePhonePad)
+                                    .frame(width: screenWidth - 50, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 1)
+                                    .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+                                    .multilineTextAlignment(.center)
+                                
+                                Button(action: { self.showsAlert.toggle() }) {
+                                    Text("Calculate")
+                                }
+                                .alert(isPresented: self.$showsAlert) {
+                                    Alert(title: Text("\(CalculatePercentage(total: fNum, percentage: lNum))"))
+                                }
+                                
                             }
-                            .alert(isPresented: self.$showsAlert) {
-                                Alert(title: Text("\(CalculatePercentage(total: fNum, percentage: lNum))"))
-                            }
-                            
+                            .frame(width: screenWidth - 45, height: 160, alignment: .top)
                         }
-                        .frame(width: screenWidth - 45, height: 160, alignment: .top)
-                    }
-                    
-                    Group {
-                        Text("Find Percentage")
-                            .bold()
-                            .font(.system(size: 20))
                         
-                        Text("Find out how much percentage the value covers")
-                            .font(.system(size: 15))
-                        
-                        VStack(alignment: .center, spacing: 10) {
-                            TextField("Value", text: $Value)
-                                .keyboardType(.namePhonePad)
-                                .frame(width: screenWidth - 50, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 1)
-                                .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
-                                .multilineTextAlignment(.center)
+                        Group {
+                            Text("Find Percentage")
+                                .bold()
+                                .font(.system(size: 20))
                             
-                            TextField("Total Amount", text: $TotalAmount)
-                                .keyboardType(.namePhonePad)
-                                .frame(width: screenWidth - 50, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 1)
-                                .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
-                                .multilineTextAlignment(.center)
+                            Text("Find out how much percentage the value covers")
+                                .font(.system(size: 15))
                             
-                            Button(action: { self.showSecondAlert.toggle() }) {
-                                Text("Calculate")
+                            VStack(alignment: .center, spacing: 10) {
+                                TextField("Value", text: $Value)
+                                    .keyboardType(.namePhonePad)
+                                    .frame(width: screenWidth - 50, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 1)
+                                    .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+                                    .multilineTextAlignment(.center)
+                                
+                                TextField("Total Amount", text: $TotalAmount)
+                                    .keyboardType(.namePhonePad)
+                                    .frame(width: screenWidth - 50, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 1)
+                                    .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+                                    .multilineTextAlignment(.center)
+                                
+                                Button(action: { self.showSecondAlert.toggle() }) {
+                                    Text("Calculate")
+                                }
+                                .alert(isPresented: self.$showSecondAlert) {
+                                    Alert(title: Text("\(CalculateValue(total: TotalAmount, value: Value))"))
+                                }
+                                
                             }
-                            .alert(isPresented: self.$showSecondAlert) {
-                                Alert(title: Text("\(CalculateValue(total: TotalAmount, value: Value))"))
-                            }
-                            
+                            .frame(width: screenWidth - 45, height: 100, alignment: .center)
                         }
-                        .frame(width: screenWidth - 45, height: 100, alignment: .center)
+                        
                     }
-                    
+                    .frame(width: screenWidth - 35, height: screenHeight - 40, alignment: .topLeading)
+                    .border(Color.green, width: 1)
                 }
-                .frame(width: screenWidth - 35, height: screenHeight - 40, alignment: .topLeading)
-                .border(Color.green, width: 1)
-            }
-            .frame(width: screenWidth, height: screenHeight)
-            .border(Color.black, width: 1)
-        )
+                .frame(width: screenWidth, height: screenHeight)
+                .border(Color.black, width: 1)
+                .ignoresSafeArea(.keyboard, edges: .all)
+            )
+            demoView()
+        }
     }
 }
